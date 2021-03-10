386d6d9da62a8575b351b664598ecc644ec686a6
ryanskydude <ryanskydude@gmail.com>
2021-03-09 17:51:11 -0800

    some fuckin statistics and attributes that dont work
    lmao

---
+++ Assets/Character.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatsContainer
+{
+    public List<ValueReference> valueList;
+
+    public StatsContainer()
+    {
+        valueList = new List<ValueReference>();
+    }
+
+    public void Sum(Value v, int Sum)
+    {
+        int i = valueList.FindIndex(x => x.valueBase == v);
+        if (i != -1)
+        {
+            ValueIntReference reference = (ValueIntReference)valueList[i];
+            reference.Sum(sum);
+        }
+        else
+        {
+            Add(v, sum);
+        }
+    }
+
+    public void Sum(Value v, float Sum)
+    {
+        int i = valueList.FindIndex(x => x.valueBase == v);
+        if (i != -1)
+        {
+            ValueFloatReference reference = (ValueFloatReference)valueList[i];
+            reference.Sum(sum);
+        }
+        else
+        {
+            Add(v, sum);
+        }
+    }
+
+    private void Add(Value v, float sum)
+    {
+        valueList.Add(new ValueFloatReference(v, sum));
+    }
+    private void Add(Value v, int sum)
+    {
+        valueList.Add(new ValueIntReference(v, sum));
+    }
+}
+
+
+
+
+
+
+
+  
+
+public class Character : MonoBehaviour
+{
+    public ValueStructure statsStructure;
+    StatsContainer statsContainer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();  
+    }
+
+    void Init()
+    {
+        statsContainer = new StatsContainer();
+        for (int i = 0; i < statsStructure.values.Count; i++)
+        {
+            if (statsStructure.values[i] is ValueFloat)
+            {
+                statsContainer.valueList.Add(new ValueFloatReference(statsStructure.values[i], 0f));
+            }
+            if (statsStructure.values[i] is ValueInt)
+            {
+                statsContainer.valueList.Add(new ValueIntReference(statsStructure.values[i], 0));
+            }
+        }
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnGUI()
+    {
+        for(int i = 0; i < statsContainer.valueList.Count; i++)
+        {
+            GUI.Label(new Rect(10, 10 + 30 * i, 500, 22), statsContainer.valueList[i].valueBase.Name);
+        }
+    }
+}
